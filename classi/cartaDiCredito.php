f253d96010ac8b5b4f2a5a332cea341df07f400b
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-16 16:53:07 +0200

    modifiche in pagamenti

--- classi/cartaDiCredito.php
+++ classi/cartaDiCredito.php
@@ -4,9 +4,17 @@
 class pagamenti extends utenti
 {
 
-    public $nomeTitolare;
     public $numeroCarta;
     public $iban;
     public $cvv;
     public $scadenza;
+
+
+    function __construct($_numeroCarta, $_iban, $_cvv, $_scadenza)
+    {
+        $this->numeroCarta = $_numeroCarta;
+        $this->iban = $_iban;
+        $this->cvv = $_cvv;
+        $this->scadenza = $_scadenza;
+    }
 }
