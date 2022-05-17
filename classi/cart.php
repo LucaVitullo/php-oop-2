9c95fcf0db5195c4999e66b39b5823f677b3aea9
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-17 14:34:36 +0200

    sistemato durante la lezione

---
+++ classi/cart.php
@@ -0,0 +1,48 @@
+<?php
+
+include_once __DIR__ . '/user.php';
+include_once __DIR__ . '/user_registered.php';
+
+class Cart
+{
+
+    public $products = [];
+    public $user;
+
+    private $isRegisteredUser = false;
+
+    function __construct()
+    {
+        $this->user = new User();
+    }
+
+    public function setRegisteredUser($_user)
+    {
+        $this->user = $_user;
+        $this->isRegisteredUser = true;
+    }
+
+    public function addProduct($_product)
+    {
+        $this->products[] = $_product;
+    }
+
+    public function emptyCart()
+    {
+        $this->products = [];
+    }
+
+    public function checkOut($sum)
+    {
+        // somma tutti i prezzi dei prodotti nel carrello
+        // se un user ha uno sconto lo applico 
+        // da qui poi potrò prendere il pagamento
+        foreach ($this->products as $product) {
+            $sum += $product->getPrice();
+        }
+
+        $discount = $this->user->getDiscount();
+
+        return $sum - ($sum * $discount / 100);
+    }
+}
--- index.php
+++ index.php
@@ -1,44 +1,47 @@
+<!-- immagina le classi necessarie per creare uno shop online con le seguenti caratteristiche.
+L'e-commerce vende prodotti per gli animali.
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
+Il pagamento avviene con la carta di credito, che non deve essere scaduta.
+Dividete bene in classi e implementate gli attributi e i metodi necessari per il corretto funzionamento dell'e-commerce. 
+Il focus è sulla parte di slide condivisa oggi su Drive.
+BONUS:
+Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibili solo in un periodo particolare (es. da maggio ad agosto) -->
+<?php
+
+include_once 'classi/users.php';
+
+function var_dump_pre($showOff)
+{
+    echo '<pre>';
+    var_dump($showOff);
+    echo '</pre>';
+};
+
+$user1 = new User();
+
+
+?>
+
 <!DOCTYPE html>
-<html lang="it">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OOP-2</title>
+    <link rel="icon" href="https://boolean.careers/favicon/favicon.ico">
+    <title>PHP oop-2</title>
 </head>
 
 <body>
-    <?php
-    /*Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche.
-L'e-commerce vende prodotti per gli animali.
-I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
-L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
-Il pagamento avviene con la carta di credito, che non deve essere scaduta.
-Dividete bene in classi e implementate gli attributi e i metodi necessari per il corretto funzionamento dell'e-commerce.
-Il focus è sulla parte di slide condivisa oggi su Drive.
-BONUS:
-Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibili solo in un periodo particolare (es. da maggio ad agosto).*/
+    <h1>E-Commerce</h1>
+    <div class="e-commerce">
+        <?php
+        var_dump_pre($user1);
 
-    include 'classi/pagamenti.php.';
-    include 'classi/prodotti.php';
-    include 'classi/utente.php';
-
-
-
-    $utente = new Utente('Pippo', 'Baudo', 'PPOBDO70T02F412F');
-
-    echo "<pre>";
-    var_dump($utente);
-    echo "<pre>";
-
-
-    $prodotto = new prodotto('collare', 'k-9', 'collare a strozzo in pelle vera', '15,80$');
-    echo "<pre>";
-    var_dump($prodotto);
-    echo "<pre>";
-
-    ?>
+        ?>
+    </div>
 </body>
 
 </html>
